0e0eac9b2fac194d0ea1bd697abefc698d60ee98
dahtalebabeidy <dahtalebabeidy@gmail.com>
2023-06-17 02:29:46 +0000

    s

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -22,7 +22,7 @@ int main(void)
 				if (digit1 != digit2 &&
 				    digit1 < digit2 &&
 				    digit2 != digit3 &&
-				    digit2 != digit3)
+				    digit2 < digit3)
 				{
 					putchar(digit1 + 48);
 					putchar(digit2 + 48);
