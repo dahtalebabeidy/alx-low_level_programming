e8998a7aa3101b1c6ed38150913ac97038183d36
dahtalebabeidy <dahtalebabeidy@gmail.com>
2023-06-24 01:35:27 +0000

    s

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_diagonal - print a diagonal line
+*
+* @n: input for function
+*/
+
+void print_diagonal(void)
+{
+	int postn, space;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (postn = 1; postn <= n; postn++)
+		{
+			for (space = 1; space <= postn; space++)
+				_putchar('');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
