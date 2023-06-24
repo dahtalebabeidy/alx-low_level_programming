d0eb4a28a20b54b63a169e3621fa456cd0c9d3f0
dahtalebabeidy <dahtalebabeidy@gmail.com>
2023-06-24 00:40:57 +0000

    s

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_numbers - print 0 - 9
+*
+* Return: always 0 (succes)
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
