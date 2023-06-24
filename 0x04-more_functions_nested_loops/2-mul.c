1c17088512a63eb0521631e307f544d0b78e4304
dahtalebabeidy <dahtalebabeidy@gmail.com>
2023-06-24 00:27:24 +0000

    s

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* mult - multiples two integers
+*
+* @a: first input for function
+* @b: second input for function
+*
+* Return: a * b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
