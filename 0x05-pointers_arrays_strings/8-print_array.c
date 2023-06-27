6967265f6fb1c96dea6df50fc72d2bae4d8a5627
dahtalebabeidy <dahtalebabeidy@gmail.com>
2023-06-27 19:09:15 +0000

    s

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_array - function that prints array
+*
+* @a: first paramtre
+* @n: second paramtre
+*
+* Return: Nothing
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	_putchar('\n');
+}
