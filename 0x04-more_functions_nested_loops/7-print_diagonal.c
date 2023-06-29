115d9063ca26ea2bc9873df5557e3ebc4e809eb8
dahtalebabeidy <dahtalebabeidy@gmail.com>
2023-06-29 14:19:21 +0000

    s

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,11 +16,15 @@ void print_diagonal(int n)
 		_putchar('\n');
 	else
 	{
-		for (p = 1; p <= n; p++)
+		for (p = 0; p < n; p++)
 		{
-			for (s = 1; s <= p; s++)
-				_putchar(' ');
-			_putchar(92);
+			for (s = 0; s < n; s++)
+			{
+				if (s == p)
+					_putchar('\\');
+				else
+					_putchar(' ');
+			}
 			_putchar('\n');
 		}
 	}
