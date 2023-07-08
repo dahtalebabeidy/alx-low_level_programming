7fdf1f48ff434c25ba650b46fea0923fafc5f0ff
dahtalebabeidy <dahtalebabeidy@gmail.com>
2023-07-08 01:30:36 +0000

    s

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+* main - Entry point
+*
+* @argc: int
+* @argv: int
+*
+* Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	int i = 0;
+
+	while (argc--)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+
+	return (0);
+}
