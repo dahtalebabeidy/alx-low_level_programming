7a582f26c9d33c65b50f89aab6827f61bb26e32e
dahtalebabeidy <dahtalebabeidy@gmail.com>
2023-07-08 01:36:11 +0000

    s

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* @argc: int
+* @argv: int
+*
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc == 3)
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
