a790cf280c9f8a894fde3e1cce779b1d3f124844
dahtalebabeidy <dahtalebabeidy@gmail.com>
2023-07-18 22:57:26 +0000

    D

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+
 /**
 * struct dog - a dog struct
 *
@@ -17,5 +19,6 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
