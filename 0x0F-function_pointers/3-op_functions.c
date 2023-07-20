8d6ee0d33105335203588a2dbba505b946a8ec53
dahtalebabeidy <dahtalebabeidy@gmail.com>
2023-07-20 19:57:11 +0000

    s

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-calc.h"
+
+/**
+* op_add - adds two integers
+*
+* @a: parametre
+* @b: parametre
+*
+* Return: sum
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* op_sub - sub two integers
+*
+* @a: parametre
+* @b: parametre
+*
+* Return: sub
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - mult two integers
+*
+* @a: parametre
+* @b: parametre
+*
+* Return: mul
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - div two integers
+*
+* @a: parametre
+* @b: parametre
+*
+* Return: div
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - mod two integers
+*
+* @a: parametre
+* @b: parametre
+*
+* Return: mod
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
