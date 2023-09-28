d08790e927bcac6a05b605f11b35369ec1e92d70
dahtalebabeidy <dahtalebabeidy@gmail.com>
2023-09-28 18:24:48 +0000

    s

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - flip bits
+ * @n: param
+ * @m: param
+ *
+ * Return: unsigned int
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int x = n ^ m;
+	unsigned int count = 0;
+
+	while (x)
+	{
+		if (x & 1ul)
+			count++;
+		x = x >> 1;
+	}
+
+	return (count);
+}
